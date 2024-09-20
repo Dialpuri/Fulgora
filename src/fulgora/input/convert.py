b1a3e749eb62c3b7103b6c3095e1b7ef3d4f2a42
Jordan Dialpuri <dialpuri.jordan@gmail.com>
2024-09-20 22:42:35 +0100

    Added fchk and output parsing

--- src/fulgora/input/convert.py
+++ src/fulgora/input/convert.py
@@ -4,7 +4,7 @@ from typing import List
 from types import SimpleNamespace
 
 
-def format_coord(coordinate: float | str): 
+def format_coord(coordinate: float | str):
     coordinate = float(coordinate)
     return f"{coordinate:9.6f}"
 
@@ -12,7 +12,15 @@ def format_coord(coordinate: float | str):
 def cif_to_xyz(cif_path: str | Path) -> List[dict]:
     document = gemmi.cif.read(str(cif_path))
     block = document.sole_block()
-    chemcomp_atoms = block.find("_chem_comp_atom.", ["type_symbol", "pdbx_model_Cartn_x_ideal", "pdbx_model_Cartn_y_ideal", "pdbx_model_Cartn_z_ideal"])
+    chemcomp_atoms = block.find(
+        "_chem_comp_atom.",
+        [
+            "type_symbol",
+            "pdbx_model_Cartn_x_ideal",
+            "pdbx_model_Cartn_y_ideal",
+            "pdbx_model_Cartn_z_ideal",
+        ],
+    )
     xyz = []
     for atom in chemcomp_atoms:
         x = format_coord(atom[1])
@@ -20,18 +28,19 @@ def cif_to_xyz(cif_path: str | Path) -> List[dict]:
         z = format_coord(atom[3])
         data = SimpleNamespace(**{"element": atom[0], "x": x, "y": y, "z": z})
         xyz.append(data)
-    
+
     return xyz
 
 
-def xyz_to_gjf_atoms(xyz: List[dict]) -> str: 
+def xyz_to_gjf_atoms(xyz: List[dict]) -> str:
     return_string = ""
-    
-    for atom in xyz: 
+
+    for atom in xyz:
         return_string += f"{atom.element}     {atom.x}   {atom.y}   {atom.z}\n"
-    
+
     return return_string
-    
+
+
 def cif_to_gjf_atoms(cif_path: str | Path) -> str:
     xyz = cif_to_xyz(cif_path=cif_path)
     return xyz_to_gjf_atoms(xyz=xyz)
--- src/fulgora/util/directory.py
+++ src/fulgora/util/directory.py
@@ -2,13 +2,16 @@ from pathlib import Path
 from datetime import datetime
 
 
-def create(args) -> Path: 
+def create(args) -> Path:
     output_dir = Path(args.outdir)
+    if args.dev:
+        return output_dir
+
     output_dir.mkdir(exist_ok=True, parents=True)
     time = datetime.now()
 
-    formatted_model = Path(args.model).stem
-    run_dir = output_dir / f"{formatted_model}_{time.strftime('%d_%m_%Y-%H:%M:%S')}"
+    name = Path(args.model).stem if not args.name else args.name
+    run_dir = output_dir / f"{name}_{time.strftime('%d_%m_%Y-%H:%M:%S')}"
 
     run_dir.mkdir(exist_ok=True)
     return run_dir
